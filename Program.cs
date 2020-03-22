53683104b85c57f21751c198a99e3f885a15d0f7
countrysidekid <62136604+countrysidekid@users.noreply.github.com>
2020-03-23 05:38:07 +1400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace substraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = 0;
+            int secondNumber = 0;
+            int result = 0;
+
+            Console.Write("please enter the first number: ");
+            firstNumber = int.Parse(Console.ReadLine());
+
+            Console.Write("please enter the second number:");
+            secondNumber = int.Parse(Console.ReadLine());
+
+            result = firstNumber - secondNumber;
+            Console.WriteLine("the answer is " + result );
+        }
+    }
+}
