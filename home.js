f17f189817cf172c51a23bcc3b105fe9d3c128f1
countrysidekid <62136604+countrysidekid@users.noreply.github.com>
2020-03-25 00:51:39 +1400

    Add files via upload

---
+++ home.js
@@ -0,0 +1,11 @@
+function
+var name = "chinelo ikenna";
+console.log(name);
+
+var courses = "csharp, design, html,css,js, kotlin, nodejs, php, python,ruby";
+console.log(courses);
+
+var i;
+for (i = 0; i < 200; i++)
+if ((i % 2) == 0)
+console.log(i);
